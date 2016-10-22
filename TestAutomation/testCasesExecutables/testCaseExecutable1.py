b7e0d5221233def8f7354d4512a6fa5985dc69ab
JMCCMJ <carringtonjo@g.cofc.edu>
2016-10-22 13:00:15 -0400

    Create testCaseExecutable1.py

---
+++ TestAutomation/testCasesExecutables/testCaseExecutable1.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import unittest
+import sys
+
+class TestLineMethods(unittest.TestCase):
+	color = None
+	#width = None
+	expectedColor = 'red'
+	#expectedWidth = 5
+	
+	def setUp(self):
+		line, = plt.plot([1,2,3,4])
+
+	
+	def test_line_color(self):
+		line, = plt.plot([1,2,3,4])
+		line.set_color(self.color)
+		#line.set_linewidth(5)
+		self.assertEqual(line.get_color(),self.expectedColor)
+
+	"""def test_line_width(self):
+		#line, = plt.plot([1,2,3,4])
+		#line.set_color('red')
+		line, = plt.plot([1,2,3,4])
+		line.set_linewidth(self.width)
+		self.assertEqual(int(line.get_linewidth()),int(self.width))"""
+
+
+if __name__=='__main__':
+	#import sys
+	
+	if len(sys.argv) > 1:
+        	TestLineMethods.color = sys.argv.pop()
+		TestLineMethods.expectedColor = sys.argv.pop()
+        	#TestLineMethods.width = sys.argv.pop()
+	unittest.main()
