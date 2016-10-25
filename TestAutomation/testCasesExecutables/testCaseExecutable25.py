60cb7fbf9cfd64ad57f3af3b25e8aec948ca349f
pdubcali <wilsonpm@g.cofc.edu>
2016-10-24 19:41:25 -0700

    Add files via upload

---
+++ TestAutomation/testCasesExecutables/testCaseExecutable25.py
@@ -0,0 +1,21 @@
+numpy as np
+import matplotlib.colors as colors
+import unittest
+import sys
+
+class TestColorMethods(unittest.TestCase):
+    color = None
+    expectedRGB = None
+	
+    def test_hex2color(self):		
+		
+        self.assertEqual(str(colors.hex2color(self.color)), self.expectedRGBA)
+		
+
+if __name__=='__main__':
+    #import sys
+	
+    if len(sys.argv) > 1:
+    TestColorMethods.color = sys.argv.pop()
+    TestColorMethods.expectedRGBA = sys.argv.pop()
+    unittest.main()
