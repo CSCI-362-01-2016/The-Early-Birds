92f0a03df35f8700d52c1a0e73f6aca665378650
pdubcali <wilsonpm@g.cofc.edu>
2016-10-24 18:54:54 -0700

    Add files via upload

---
+++ TestAutomation/testCasesExecutables/testCaseExecutable19.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.colors as colors
+import unittest
+import sys
+
+class TestColorMethods(unittest.TestCase):
+	color = None
+	expectedHex = None
+	
+	def test_to_hex(self):
+				
+		self.assertEqual(colors.to_hex(self.color), self.expectedHex)
+		
+
+if __name__=='__main__':
+	#import sys
+	
+	if len(sys.argv) > 1:
+        	TestColorMethods.color = sys.argv.pop()
+		TestColorMethods.expectedHex = sys.argv.pop()
+        	
+	unittest.main()
