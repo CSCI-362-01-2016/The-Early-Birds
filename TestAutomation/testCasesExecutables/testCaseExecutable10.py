69abf6294beb7475df0a4abee21c2749af036a56
Vincent Nguyen <GenKiPS@Gmail.com>
2016-11-14 10:56:48 -0500

    Add files via upload

--- TestAutomation/testCasesExecutables/testCaseExecutable10.py
+++ TestAutomation/testCasesExecutables/testCaseExecutable10.py
@@ -6,18 +6,15 @@ import sys
 
 class TestDatesMethods(unittest.TestCase):
 	temp = None
-	temp2 = None
-	expectedValue = 735731.0416666666
+	expectedValue = None
 	
 	def test_drange(self):
-		temp = self.temp
-		temp2 = self.temp2
-		temp = temp.split(" ")
-		temp2 = temp2.split(" ")	
+		temp = self.temp		
+		temp = temp.split(" ")			
 		x = datetime.datetime(int(temp[0]), int(temp[1]), int(temp[2]))
-		y = datetime.datetime(int(temp2[0]), int(temp2[1]), int(temp2[2]))
+		y = datetime.datetime(int(temp[3]), int(temp[4]), int(temp[5]))
 		delta = datetime.timedelta(hours = 5)		
-		self.assertEqual(dates.drange(x,y,delta)[5], self.expectedValue)
+		self.assertEqual(dates.drange(x,y,delta)[5], float(self.expectedValue))
 		
 
 if __name__=='__main__':
@@ -25,6 +22,6 @@ if __name__=='__main__':
 	
 	if len(sys.argv) > 1:
         	TestDatesMethods.temp = sys.argv.pop()
-		TestDatesMethods.temp2 = sys.argv.pop()	
+		TestDatesMethods.expectedValue = sys.argv.pop()	
 		       	
 	unittest.main()
