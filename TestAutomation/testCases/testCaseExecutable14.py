a58b60186dca28ba464c555ff70ad1965d12ade7
JMCCMJ <carringtonjo@g.cofc.edu>
2016-11-14 21:05:37 -0500

    Add files via upload

---
+++ TestAutomation/testCases/testCaseExecutable14.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import unittest
+import sys
+import matplotlib.dates as dates
+import datetime
+import matplotlib
+
+class TestLineMethods(unittest.TestCase):
+	
+	weeks = None
+	expected_days = None
+	
+	def setUp(self):
+		line, = plt.plot([1,2,3,4])
+
+	
+	def test_matplotlib_dates_seconds(self):
+		
+		
+		self.assertEqual(float(self.expected_days),float(matplotlib.dates.weeks(self.weeks)))
+
+	"""def test_line_width(self):
+		#line, = plt.plot([1,2,3,4])
+		#line.set_color('red')
+		line, = plt.plot([1,2,3,4])
+		line.set_linewidth(self.width)
+		self.assertEqual(int(line.get_linewidth()),int(self.width))"""
+
+
+if __name__=='__main__':
+	#import sys
+	
+	if len(sys.argv) > 1:
+        	TestLineMethods.weeks = sys.argv.pop()
+		TestLineMethods.expected_days = sys.argv.pop()
+        	#TestLineMethods.width = sys.argv.pop()
+	unittest.main()
